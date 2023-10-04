8313907f73a216fd6651c909d8e2a8777edfa3b7
FunkierCoder FUNKIER <contactosjuandm@gmail.com>
2023-10-03 22:39:59 -0700

    Agregar archivos de proyecto.

---
+++ Metodos.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Metodos
+    {
+        public void Arreglos()
+        {
+            int[] arregloNumerico = new int[5];
+            for (int i = 0; i < arregloNumerico.Length; i++)
+            {
+                Console.WriteLine($"Ingresar numero  {i + 1}");
+                arregloNumerico[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            foreach (int muestra in arregloNumerico)
+            {
+                Console.WriteLine("Los numeros son"+muestra);
+            }
+
+        }
+
+        public void Media(int tamaño)
+        {
+            int[]arreglo = new int[10] {2,2,2,2,2,2,2,2,2,2};
+           
+            int suma = 0;
+            foreach (int valor in arreglo)
+            {
+                suma += valor;
+            }
+            double media = suma / arreglo.Length;
+            foreach (int valor in arreglo)
+            {
+                Console.WriteLine(valor+"");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Media de los valores del arreglo "+ media);
+        }
+        public void Multiplos()
+        {
+            Console.WriteLine("Ingresa la longitud de tu arreglo");
+            int tamaño = Convert.ToInt32(Console.ReadLine());
+            int[] arreglob = new int[tamaño];
+         
+            Console.WriteLine("ingresa numero para la funcion del arreglo");
+            int numeroMultiplo = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= tamaño; i++)
+            {
+                Console.WriteLine($" Los numero multiplo son: {numeroMultiplo * i}");
+            }
+
+
+        }
+        public void Cadenas()
+        {
+            Console.WriteLine("Ingresa tamaño de los arreglos");
+            int tamaño = Convert.ToInt32(Console.ReadLine());
+
+            string[] nombre = new string[tamaño];
+            int[] longitud = new int[tamaño];
+            for (int i = 0; i< tamaño;i++)
+            {
+                Console.WriteLine($"ingrese nombre {i +1}");
+                nombre[i] = Console.ReadLine();
+                longitud[i] = nombre[i].Length;
+
+            }
+          for (int i = 0; i< tamaño; i++)
+            {
+                Console.WriteLine($"EL nombre es : {nombre[i]} y su numero de palabras son {longitud[i]}");
+
+            }
+         
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodos metodos = new Metodos();
+
+            //metodos.Arreglos();
+
+            //int tamaño = 0;
+            //metodos.Media(tamaño);
+
+            //metodos.Multiplos();
+
+            //metodos.Cadenas();
+        }
+    }
+}
